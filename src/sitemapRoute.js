8c13e998f7f94b4faaade4019b61531146346d6d
balhyo-younjisang <yjs12180825@gmail.com>
2022-12-28 10:31:04 +0900

    SEO

--- src/sitemapRoute.js
+++ src/sitemapRoute.js
@@ -4,15 +4,7 @@ import { Route, Switch } from 'react-router';
 export default (
     <Switch>
         <Route path={'/'} />
-        <Route path={'/introduce'} />
-        <Route path={'/blog'} />
-        <Route path={'/joinus'} />
-        <Route path={'/joinus/:id'} />
-        <Route path={'/conduct'} />
-        <Route path={'/question'} />
-        <Route path={'/signin'} />
-        <Route path={'/signup'} />
-        <Route path={'/apply'} />
-        <Route path={'/admin'} />
+        <Route path={'/about'} />
+        <Route path={'/gallery'} />
     </Switch>
 );
